a0d2862a5dc37f8712432072acca2e182d9dc09d
Uriel Campos <urielcampos95@gmail.com>
2022-02-11 17:46:14 -0300

    Create HTTP Api router

---
+++ lib/api/router.ex
@@ -0,0 +1,23 @@
+defmodule HackerNewsAggregatorEx.Api.Router do
+  use Plug.Router
+
+  alias HackerNewsAggregatorEx.Api.Controllers.TopStories
+  alias HackerNewsAggregatorEx.Api.Controllers.Story
+
+  plug Plug.Logger
+  plug :match
+
+  plug :dispatch
+
+  get "/top_stories" do
+    TopStories.index(conn, opts)
+  end
+
+  get "/story/:id" do
+    Story.show(conn, opts)
+  end
+
+  match _ do
+    send_resp(conn, 404, "Oops!")
+  end
+end
