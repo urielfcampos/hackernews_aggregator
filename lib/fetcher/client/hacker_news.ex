4291226e0bdb29347737871a623ae09cd42563f6
Uriel Campos <urielcampos95@gmail.com>
2022-02-11 17:58:31 -0300

    Improve HackerNews Client

--- lib/fetcher/client/hacker_news.ex
+++ lib/fetcher/client/hacker_news.ex
@@ -3,26 +3,31 @@ defmodule HackerNewsAggregatorEx.Fetcher.HackerNews do
 
   alias HackerNewsAggregatorEx.Utils
 
-  plug Tesla.Middleware.BaseUrl, "https://hacker-news.firebaseio.com/v0/"
-  plug Tesla.Middleware.Headers
-  plug Tesla.Middleware.JSON
+  plug(Tesla.Middleware.BaseUrl, "https://hacker-news.firebaseio.com/v0/")
+  plug(Tesla.Middleware.Headers)
+  plug(Tesla.Middleware.JSON)
 
   def get_top_stories do
     case get("/topstories.json") do
       {:ok, %{status: 200, body: body}} ->
-        result = Enum.take(body, 50)
-        |> Enum.map(&get_stories/1)
+        result =
+          Enum.take(body, 50)
+          |> Enum.map(&get_stories/1)
+
         Utils.paginate(result)
+
       {:ok, %{status: status}} ->
         {:error, status}
     end
   end
 
   def get_stories(id) do
-    IO.puts("fetcing story #{id}")
+    IO.puts("fetching story #{id}")
+
     case get("/item/#{id}.json") do
       {:ok, %{status: 200, body: body}} ->
-        body
+        Map.put(body, :api_uri, "/story/#{id}")
+
       {:ok, %{status: status}} ->
         {:error, status}
     end
